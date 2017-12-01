50b54e661eb7a6c17d022af95dfcb46bd8a38670
Eetu Häivälä <eetu.haivala@gmail.com>
2017-12-01 11:08:16 +0200

    Update wo.fish to match the new Project environment structure

--- fish/functions/wo.fish
+++ fish/functions/wo.fish
@@ -1,11 +1,11 @@
-function wo -d "Activate a virtualenv"
+function wo -d "Work on a project"
     if test (count $argv) -lt 1
-        echo "Usage: wo <environment name>"
+        echo "Usage: wo <project name>"
         return 1
     end
     set -l envname $argv[1]
-    set -l envdir ~/envs/$envname/venv
-    set -l projdir ~/envs/$envname
+    set -l envdir ~/Projects/$envname/venv
+    set -l projdir ~/Projects/$envname
     if set -q VIRTUAL_ENV
         echo "Deactivating current virtualenv $VIRTUAL_ENV"
         deactivate
@@ -14,13 +14,13 @@ function wo -d "Activate a virtualenv"
         echo "Activating $envdir"
         source $envdir/bin/activate.fish
     else
-        echo "No such env."
+        echo "Project has no virtualenv."
     end
     if test -d $projdir
         echo "Changing to project directory $projdir"
         cd $projdir
     end
-    if test -d $projdir/project
-        cd project
+    if test -d $projdir/app
+        cd app
     end
 end
